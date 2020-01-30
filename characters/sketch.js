4e446d7662e41bcc5c2ce353f2826274a3e0de2f
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2020-01-29 19:08:08 -0500

    ok

--- characters/sketch.js
+++ characters/sketch.js
@@ -1,8 +1,36 @@
 
+
+var miniL;  
+var miniR;
+
+function preload() {
+	 miniL = loadImage('MinionsL.png');
+	 miniR = loadImage('MinionsR.png');
+}
+
+var miniLX =200;  
+var miniLY =150;
+
+
+var miniRX =600;  
+var miniRY =150;
+ 
+var story = "Finally Bob and kevein found each other.";
+
 function setup() { 
     createCanvas(windowWidth, windowHeight);
 
 }
 function draw() {
-    background
+    background('#fffff0');
+
+	// draw characters
+	image(miniL, miniLX, miniLY);
+	image(miniR, miniRX, miniRY);
+
+	// narration
+    fill('coral');
+	textSize(40);
+	textAlign(CENTER, CENTER);
+	text(story, width/5, 20, width/3);
 }
