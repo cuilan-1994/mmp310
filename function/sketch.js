182090bf986568a2d164dcaabed65f77f963f104
Lan.cui <lancui@lans-mbp.bmcc.cuny.edu>
2020-02-19 17:55:47 -0500

    ok

--- function/sketch.js
+++ function/sketch.js
@@ -6,7 +6,8 @@ var boss;
 function preload() {
 	 miniL = loadImage('MinionsL.png');
 	 miniR = loadImage('MinionsR.png');
-     boxx = loadImage('boss.jpg');
+     boss = loadImage('boss.jpg');
+     mo=loadImage('Mountain.jpg');
 }
 
 var miniLX =200;  
@@ -19,15 +20,23 @@ var miniRY =150;
 var bossX =500;  
 var bossY =150;
  
-var story = "Finally Bob and kevein found each other.";
-
+var story = "Finally Bob and kevin found each other.";
+//var curruntSetting ="";
 function setup() { 
     createCanvas(windowWidth, windowHeight);
 
 }
-function draw() {
+function mousePressed() {
+    if (currentSetting == "road") {
     background('#fffff0');
-
+        } 
+    else if (currentSetting == "Mountain") {
+ background('mo');
+    }
+    else if (currentSetting == "foundboss") {
+        background('#fffff0');
+        image (boss,width/2, height * 2/3)
+    }
 	// draw characters
 	image(miniL, miniLX, miniLY);
 	image(miniR, miniRX, miniRY);
