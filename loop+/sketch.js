a687733fefe6e451bdaeead6fc114536c329baf3
cuilan-1994 <55152655+cuilan-1994@users.noreply.github.com>
2020-03-02 19:00:51 -0500

    Add files via upload

---
+++ loop+/sketch.js
@@ -0,0 +1,70 @@
+
+
+var miniL;  
+var miniR;
+var tree;  
+var brid;
+
+
+function preload() {
+	 miniL = loadImage('MinionsL.png');
+	 miniR = loadImage('MinionsR.png');
+     tree= loadImage('tree.png');
+    brid= loadImage('brid.png');
+                   
+}
+
+var miniLX =200;  
+var miniLY =150;
+
+
+var miniRX =800;  
+var miniRY =150;
+
+
+ 
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	setting();
+}
+
+function mousePressed() {
+	setting();
+}
+
+
+function draw() {
+  
+
+	// draw characters
+	image(miniL, miniLX, miniLY);
+	image(miniR, miniRX, miniRY);
+
+	
+}
+function setting() {
+	// background colors
+	background('lightblue');
+
+	noStroke();
+	fill('sandybrown');
+	rect(0, height/2, width, height/2);
+
+	
+	// trees
+	for (let x = 100; x <= width; x += 400) {
+		image(treeImage, random(width), height/2 - 150 + x/20);
+	}
+
+	
+
+	// brid
+	for (let x = -50; x <= width; x += 200) {
+		let y = random(height/3);
+		let offset = random(-100, 100);
+		image(cloudImage, x + offset, y);
+
+		// image(cloudImage, random(width), random(height));
+	}
+
+}
