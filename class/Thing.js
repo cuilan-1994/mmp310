b7241b0324d1c723a22e20899b5e17bb4ea67190
Lan.cui <lancui@Lans-MacBook-Pro.local>
2020-05-22 10:55:27 -0400

    ok

--- class/Thing.js
+++ class/Thing.js
@@ -1,11 +1,16 @@
-class Thing {
-	constructor(x, y, img) {
-		this.x = x;
-		this.y = y;
-		this.img = img;
+class Asteroid extends Thing {
+	constructor() {
+		var x = random(width);
+		var y = -100;
+		var size = random(50, 100);
+		var speedX = random(-1, 1);
+		var speedY = random(0, 3);
+		super(x, y, size, speedX, speedY);
 	}
-
-	draw() {
-		image(this.img, this.x, this.y);
+	
+	display() {
+		// draw your asteroid
+		// this.x, this.y for position
+		// this.size
 	}
 }
